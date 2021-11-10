8a796a67ad01835bf0628b2b2b49533176596c52
Victor Hoyos <bikcodeh@gmail.com>
2021-11-10 17:10:33 -0500

    implements a topic

--- app/src/main/java/com/bikcode/nilo/presentation/ui/fragment/SettingsFragment.kt
+++ app/src/main/java/com/bikcode/nilo/presentation/ui/fragment/SettingsFragment.kt
@@ -5,20 +5,31 @@ import androidx.preference.PreferenceFragmentCompat
 import androidx.preference.SwitchPreferenceCompat
 import com.bikcode.nilo.R
 import com.bikcode.nilo.presentation.util.showToast
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.messaging.ktx.messaging
 
 class SettingsFragment : PreferenceFragmentCompat() {
 
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.root_preferences, rootKey)
 
-        val switchPreferenceCompat = findPreference<SwitchPreferenceCompat>(getString(R.string.pref_offers_key))
+        val switchPreferenceCompat =
+            findPreference<SwitchPreferenceCompat>(getString(R.string.pref_offers_key))
+
+        val topic = getString(R.string.settings_topic_offers)
 
         switchPreferenceCompat?.setOnPreferenceChangeListener { preference, newValue ->
             (newValue as? Boolean)?.let { isChecked ->
-                if(isChecked) {
-                    context?.showToast(R.string.notifications_activated)
+                if (isChecked) {
+                    Firebase.messaging.subscribeToTopic(topic)
+                        .addOnSuccessListener {
+                            context?.showToast(R.string.notifications_activated)
+                        }
                 } else {
-                    context?.showToast(R.string.notifications_deactivated)
+                    Firebase.messaging.unsubscribeFromTopic(topic)
+                        .addOnSuccessListener {
+                            context?.showToast(R.string.notifications_deactivated)
+                        }
                 }
             }
             true
